7da41d0bbdd7105be563ffc419e97f961efdbc71
Rotcod X <jcbdr08@gmail.com>
2026-04-22 03:04:07 +0800

    update gitignore and main notes

--- main.py
+++ main.py
@@ -14,5 +14,9 @@ git push
 To RPi:
 cd ~/FruitShake
 git pull
+
+Virtual Environment:
+Previously: Shift + Alt + P -> Select Interpreter -> Enter interpreter path ->Select the venv manually from folders
+Try: source ~/venv-rpi/bin/activate
 """
 #endregion
