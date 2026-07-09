089bd5eede87fa420fe4cec322a02ea276176e91
Rotcod X <jcbdr08@gmail.com>
2026-07-09 15:22:37 +0800

    Add debugs for relay/servo

--- hardware.py
+++ hardware.py
@@ -248,41 +248,70 @@ class MachineController:
         self.servo_stop_value = -0.02
         self.servo_settle_time = 0.2
 
+    # 1
     def dispense_cup(self, seconds):
         print("Dispensing cup.")
         self.relays.pulse(23, seconds)
 
+    # 2
     def dispense_fruit(self, seconds):
         print(f"Dispensing fruit for {seconds}s.")
         self.relays.pulse(24, seconds)
 
+    # 3
     def dispense_ice(self, seconds):
         print(f"Dispensing ice for {seconds}s.")
         self.relays.pulse(27, seconds)
 
+    # 4
     def add_liquid(self, seconds):
         print(f"Dispensing liquid for {seconds}s.")
+        # Test
+        self.controller.log("Relay On")
         self.relays.pulse(22, seconds)
+        self.controller.log("Servo On")
         self.run_servo_for_time(self.hardware.servo1, seconds, direction=1)
+        self.controller.log("Done")
 
+    # 5
     def dispense_addons(self, seconds):
         print(f"Dispensing Add-Ons for {seconds}s.")
+        # Test
+        self.controller.log("Relay On")
         self.relays.pulse(5, seconds)
+        self.controller.log("Servo On")
         self.run_servo_for_time(self.hardware.servo2, seconds, direction=1)
+        self.controller.log("Done")
 
+    # 6
     def run_blender(self, seconds):
         print(f"Blending for {seconds}s.")
+        # Test
+        self.controller.log("Relay On")
         self.relays.pulse(6, seconds)
+        self.controller.log("Servo On")
         self.run_servo_for_time(self.hardware.servo3, seconds, direction=1)
+        self.controller.log("Done")
 
+    # 7
     def dispense_order(self, seconds):
         print(f"Dispensing order for {seconds}s.")
+        # Test
+        self.controller.log("Relay On")
         self.relays.pulse(25, seconds)
+        self.controller.log("Servo On")
         self.run_servo_for_time(self.hardware.servo4, seconds, direction=1)
+        self.controller.log("Done")
 
+    # 8
     def cleaning(self, seconds):
         print(f"Cleaning up for {seconds}s.")
+        # Test
+        self.controller.log("Relay On")
         self.relays.pulse(8, seconds)
+        self.controller.log("Servo On")
+        self.run_servo_for_time(self.hardware.servo4, seconds, direction=1)
+        self.controller.log("Done")
 
     def cleanup(self):
         self.relays.cleanup()
